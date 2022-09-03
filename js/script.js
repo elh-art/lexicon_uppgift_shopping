55523e386f4a84e1070efc094df1f1154f4ab616
elh-art <code.enfaced@aleeas.com>
2022-09-03 11:31:08 +0200

    finalization of form validation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const respErrorElem = document.getElementById('response-error')
+const respErrorElem2 = document.getElementById('response-error2')
+
 function validate(event) {
   switch(event.type) {
     case "keyup":
@@ -110,17 +113,21 @@ function handleSignUp(event) {
       })
       .then(response => {
         if (response.status === 409) {
-          document.getElementById('response-error2').innerText = 'There is already a user with the same email address'
+          respErrorElem2.innerText = 'There is already a user with the same email address'
           event.target[5].children[0].classList.add('d-none')
+          clearErrorMsg('error2')
         }
         if (response.status === 200) {
-          window.location.replace('account.html')
+          document.getElementById('response-success').innerText = 'SUCCESS! Now you can Login.'
+          event.target[5].children[0].classList.add('d-none')
+          clearErrorMsg('success')
         }
       }
      )
     } else {
-      document.getElementById('response-error2').innerText = 'Not all validations tests passed, please correct your values'
+      respErrorElem2.innerText = 'Not all validations tests passed, please correct your values'
       event.target[5].children[0].classList.add('d-none')
+      clearErrorMsg('error2')
     }
   }
   
@@ -154,23 +161,25 @@ function handleSignIn(event) {
         headers: {'Content-type':'application/json'},
         body: json
       })
-      .then(response => {
-        if (response.status === 400) {
-          document.getElementById('response-error').innerText = 'Ops! Email and password not matching.'
-          event.target[2].children[0].classList.add('d-none')
-        }
-        if (response.status === 200) {
-          window.location.replace('account.html')
-        }
+      .then(async res => {
+        if(res.status === 400) throw new Error(await res.text()) 
+        else  
+            return res.text()
       })
       .then(data => {
-        console.log(data)
         sessionStorage.setItem('token', data)
+        window.location.replace('account.html')
       })
-     
+      .catch(async error => {
+        respErrorElem.innerText = await error.message
+        event.target[2].children[0].classList.add('d-none')
+        clearErrorMsg('error')
+      })
+      
     } else {
-      document.getElementById('response-error').innerText = 'Not all validations tests passed, please correct your values'
+      respErrorElem.innerText = 'Not all validations tests passed, please correct your values'
       event.target[2].children[0].classList.add('d-none')
+      clearErrorMsg('error')
     }
 }
 
@@ -180,144 +189,13 @@ function handleSignOut() {
 }
 
 function isSignedIn() {
-  // let token = sessionStorage.getItem('token')
-  // if(token === null || token === undefined)
-  // window.location.replace('myaccount.html')
+  let token = sessionStorage.getItem('token')
+  if(token === null || token === undefined)
+  window.location.replace('myaccount.html')
 }
 
-// document.className.includes('input-control')
-
-
-// const signinForm = document.querySelector('.signin')
-// const signupForm = document.querySelector('.signup')
-
-// const firstName = document.getElementById('firstname')
-// const lastName = document.getElementById('lastname')
-// const emailSI = document.getElementById('email')
-// const emailSU = document.getElementById('emailSU')
-// const passwordSI = document.getElementById('password')
-// const passwordSU = document.getElementById('passwordSU')
-// const passwordSU2 = document.getElementById('passwordSU2')
-
-// signinForm.addEventListener('submit', e => {
-//   e.preventDefault()
-
-//   checkInputsSI()
-// })
-
-// signupForm.addEventListener('submit', e => {
-//   e.preventDefault()
-
-//   checkInputsSU()
-// })
-
-// // ### SIGNUP CHECKS: ###
-
-// function checkInputsSU() {
-//   const firstNameValue = firstName.value.trim()
-//   const lastNameValue = lastName.value.trim()
-//   const emailValue = emailSU.value.trim()
-//   const passwordValue = passwordSU.value.trim()
-//   const passwordValue2 = passwordSU2.value.trim()
-
-//   if(firstNameValue === ''){
-//       setErrorFor(firstName, 'Firstname field cannot be left empty!')  
-//     } else {
-//       setSuccessFor(firstName)
-//     }
-
-//   if(lastNameValue === ''){
-//       setErrorFor(lastName, 'Lastname field cannot be left empty!')  
-//     } else {
-//       setSuccessFor(lastName)
-//     }
-
-//   if(emailValue === ''){
-//       setErrorFor(emailSU, 'Email field cannot be left empty!')  
-//     } else if (!isEmail(emailValue)) {
-//       setErrorFor(emailSU, 'This is not a valid email address.')  
-//     } else {
-//       setSuccessFor(emailSU)
-//     }
-
-//   if(passwordValue === ''){
-//       setErrorFor(passwordSU, 'Password field cannot be left empty!')  
-//     } else if (!isPassword(passwordValue)) {
-//       setErrorFor(passwordSU, 'Minimum one lower, one upper case letter and a number. Min length: 8 characters.')
-//     } else {
-//       setSuccessFor(passwordSU)
-//     }
-
-//   if (passwordValue2 === ''){
-//     setErrorFor(passwordSU2, 'Password field cannot be left empty!')  
-//     } else if (passwordValue2 !== passwordValue) {
-//       setErrorFor(passwordSU2, 'Please, use the same password!')  
-//     } else {
-//       setSuccessFor(passwordSU2)
-//     }
-
-//   if (firstName.parentElement.classList.contains("success") &&
-//       lastName.parentElement.classList.contains('success') &&
-//       emailSU.parentElement.classList.contains('success') &&
-//       passwordSU.parentElement.classList.contains('success') &&
-//       passwordSU2.parentElement.classList.contains('success')) {
-//         window.alert('Successful registration, please check your email address for validation.')
-//         removeSuccess()
-//       }
-    
-// }
-
-// // ### SIGNIN CHECKS: ###
-
-// function checkInputsSI() {
-//   const emailValue = emailSI.value.trim()
-//   const passwordValue = passwordSI.value.trim()
-
-//   if(emailValue === ''){
-//       setErrorFor(emailSI, 'Email field cannot be left empty!')  
-//     } else if (!isEmail(emailValue)) {
-//       setErrorFor(emailSI, 'This is not a valid email address.')  
-//     } else {
-//       setSuccessFor(emailSI)
-//     }
-
-//   if(passwordValue === ''){
-//       setErrorFor(passwordSI, 'Password field cannot be left empty!')  
-//     } else if (!isPassword(passwordValue)) {
-//       setErrorFor(passwordSI, 'Minimum one lower, one upper case letter and a number. Min length: 8 characters.')
-//     } else {
-//       setSuccessFor(passwordSI)
-//     }
-
-// }
-
-// // ### COMMON FUNCTIONS: ###
-
-// function setErrorFor(input, msg) {
-//   const inputControl = input.parentElement
-//   const small = inputControl.querySelector('small')
-//   small.innerText = msg
-//   inputControl.classList.add('error')
-// }
-
-// function setSuccessFor(input) {
-//   const inputControl = input.parentElement
-//   inputControl.classList.remove('error')
-//   inputControl.classList.add('success')
-// }
-
-// function isEmail(email) {
-// 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
-// }
-
-// function isPassword(email) {
-// 	return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(email)
-// }
-
-// function removeSuccess() {
-//   firstName.parentElement.classList.remove('success')
-//   lastName.parentElement.classList.remove('success')
-//   emailSU.parentElement.classList.remove('success')
-//   passwordSU.parentElement.classList.remove('success')
-//   passwordSU2.parentElement.classList.remove('success')
-// }
+function clearErrorMsg(text) {
+  setTimeout(() => {
+    document.getElementById(`response-${text}`).innerText = ''
+  }, "3000")
+}
